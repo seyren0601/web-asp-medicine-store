fe98fcea3d1da0cce303873a5c3b3bb8627643bb
ThinhNguyen <sorrenw@gmail.com>
2024-03-20 17:29:45 +0700

    Fetch image from drugbank.vn

--- DAL/Services/Service_Thuoc.cs
+++ DAL/Services/Service_Thuoc.cs
@@ -1,4 +1,6 @@
 ﻿using Medicine_Store.DAL.Entities;
+using Newtonsoft.Json;
+using System.Collections;
 
 namespace Medicine_Store.DAL.Services
 {
@@ -13,6 +15,21 @@ namespace Medicine_Store.DAL.Services
         public List<Thuoc> GetAllThuoc()
         {
             return _context.Thuoc.ToList();
+        }
+
+        public async Task<string> GetThuocImage(Thuoc thuoc)
+        {
+            HttpClient client = new HttpClient();
+            string jsonString = await client.GetStringAsync($"https://drugbank.vn/services/drugbank/api/public/thuoc/{thuoc.thuoc_id}");
+            dynamic response = JsonConvert.DeserializeObject(jsonString);
+            if(response.images != null)
+            {
+                string imageName = response.images[0];
+                string imageUrl = "https://drugbank.vn/api/public/gridfs/" + imageName;
+                Console.WriteLine(imageUrl);
+                return imageUrl;
+            }
+            else return "";
         }
     }
 }
